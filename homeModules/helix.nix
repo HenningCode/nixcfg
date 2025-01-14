58969e3a52554ca8cd574c523cf022c0a8b63df5
HenningCode <henningwilmer@yahoo.de>
2025-01-14 23:04:26 +0100

    :)

--- homeModules/helix.nix
+++ homeModules/helix.nix
@@ -1,14 +1,30 @@
-{
-  pkgs,
-  ...
-}
-
-programs.helix = {
-  enable = true;
-  theme = "gruvbox";
-  editor = {
-    line-number = "relative";
-    lsp.display-messages = true;
-  }
-  
+{pkgs, ...}: {
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "gruvbox";
+      editor = {
+        line-number = "relative";
+        lsp.display-messages = true;
+      };
+    };
+    languages = {
+      language = [
+      {
+        name ="nix";
+        language-servers = ["nixd"];
+        formatter = {
+          command="alejandra";
+        };
+      }
+      ];
+    };
+    extraPackages = with pkgs; [
+      typescript-language-server
+      nixd
+      lua-language-server
+      stylua
+      alejandra
+    ];
+  };
 }
